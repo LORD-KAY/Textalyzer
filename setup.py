939f3822c9964b5c0c10362ac09368b5e355ad48
LORD KAY <offeilord@gmail.com>
2018-01-17 16:29:47 +0000

    Configuration for PYPI package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'textalyzer',
+	packages = ['textalyzer'],
+	version = '0.1.0',
+	description = ' A text  analyzer for repeating and contraction characters',
+	author = 'Lord Kay',
+	author_email = 'offeilord@gmail.com',
+	license = 'MIT',
+	url = 'https://github.com/LORD-KAY/Textalyzer',
+	download_url = 'https://github.com/LORD-KAY/Textalyzer/releases/0.1.0',
+	keywords = ['python','replacers','words','repeat','characters'],
+	)
