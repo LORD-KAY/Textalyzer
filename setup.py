a463b07d3f98d3afbe1ae5977a4ed5241e31d572
LORD KAY <offeilord@gmail.com>
2018-03-21 14:38:22 +0000

    Change the classifiers

--- setup.py
+++ setup.py
@@ -16,10 +16,10 @@ setup(
 	author_email = 'offeilord@gmail.com',
 	license = 'MIT',
 	classifiers=[
-          'Development Status :: v0.1.3',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT',
-          'Programming Language :: Python :: 3, Python :: 2'
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 2'
       ],
 	url = 'https://github.com/LORD-KAY/Textalyzer',
 	download_url = 'https://github.com/LORD-KAY/Textalyzer/releases/v0.1.3',
