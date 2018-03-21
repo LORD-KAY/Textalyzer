7426d914bbc18bdab4573ee747340de3b0a89298
LORD KAY <offeilord@gmail.com>
2018-03-21 14:17:45 +0000

    Change the version number

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ with open(os.path.join(base_dir,"README.rst")) as f:
 setup(
 	name = 'textalyzer',
 	packages = ['textalyzer'],
-	version = '0.1.2',
+	version = '0.1.3',
 	description = ' A text  analyzer for repeating and contraction characters',
 	long_description = long_description,
 	author = 'Lord Kay',
 	author_email = 'offeilord@gmail.com',
 	license = 'MIT',
 	url = 'https://github.com/LORD-KAY/Textalyzer',
-	download_url = 'https://github.com/LORD-KAY/Textalyzer/releases/v0.1.2',
+	download_url = 'https://github.com/LORD-KAY/Textalyzer/releases/v0.1.3',
 	keywords = ['python','replacers','words','repeat','characters'],
 	)
