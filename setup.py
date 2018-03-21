59b2d8211ec55bc45344360b5d76824c895fcd51
LORD KAY <offeilord@gmail.com>
2018-03-21 14:16:38 +0000

    Fix the long description join with the README.rst file

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
 	packages = ['textalyzer'],
 	version = '0.1.2',
 	description = ' A text  analyzer for repeating and contraction characters',
-	long_description = '',
+	long_description = long_description,
 	author = 'Lord Kay',
 	author_email = 'offeilord@gmail.com',
 	license = 'MIT',
